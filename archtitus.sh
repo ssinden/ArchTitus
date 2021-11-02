34d3b42b7dde192de5abbbb378be31a108c58f30
Shaun Sinden <ssinden@gmail.com>
2021-11-02 13:12:09 -0600

    move rsync to main script

--- archtitus.sh
+++ archtitus.sh
@@ -5,3 +5,6 @@
     source /mnt/root/ArchTitus/installs.conf
     arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/2-user.sh
     arch-chroot /mnt /root/ArchTitus/3-post-setup.sh
+    mount -t cifs -o username=ssinden@creb.ca,password=passwordhere //192.168.1.112/Drop /mnt/mnt
+	rsync --recursive /mnt/mnt/ /home/ssinden
+	umount /mnt/mnt
