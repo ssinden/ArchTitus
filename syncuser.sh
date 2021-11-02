630886c5142bca99b3c39e9576693be502926aa0
Shaun Sinden <ssinden@gmail.com>
2021-11-02 14:12:43 -0600

    fix chown

--- syncuser.sh
+++ syncuser.sh
@@ -1,3 +1,4 @@
     mount -t cifs -o username=ssinden@creb.ca,password=passwordhere //192.168.1.112/Drop /mnt/mnt
 	rsync --recursive /mnt/mnt/ /mnt/home/ssinden
 	umount /mnt/mnt
+    chown -R ssinden:ssinden /mnt/home/ssinden
